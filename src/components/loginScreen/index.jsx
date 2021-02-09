fad8a8f268c7d03ccc79a6ca89026ac8707d579b
FeSilva-dev <feliper.silva011@gmail.com>
2021-02-09 02:28:56 -0300

    Tela de login finalizada.

--- src/components/loginScreen/index.jsx
+++ src/components/loginScreen/index.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
+import './styles.css'
+import LogoDev from '../../assets/logo.svg'
 
 const LoginScreen = () => {
     return(
-        <div className="home">
-            <h1>Hello World</h1>
+        <div className="loginContent">
+            <div className="loginForm">
+                <div className="logoForm">
+                    {/* <img src={LogoDev} alt=""/> */}
+                    <p>dev<span>.</span>finance<span>$</span></p>
+                </div>
+                <div className="form">
+                    <form>
+                        <div className="input-field">
+                            <span>Email:</span>
+                            <input type="email" name="email" id="email" placeholder="Digite seu email"/>
+                        </div>
+                        <div className="input-field">
+                            <span>Password:</span>
+                            <input type="password" name="password" id="password" placeholder="Digite sua senha"/>
+                        </div>
+                        <div className="buttons">
+                            <a href="/register">Registrar</a>
+                            <button>Entrar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
